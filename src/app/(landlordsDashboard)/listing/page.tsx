1bb483010e67e29728255755bb1172da0f73e7b2
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-04-20 00:47:27 +0600

    add tenats and landloards dashboard

---
+++ src/app/(landlordsDashboard)/listing/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div> 
+      <h1>Welcome to the Landlords Dashboard listing</h1> 
+    </div>
+  )
+}
---
+++ src/app/(landlordsDashboard)/request/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>Landlords Dashboard Request Page</div>
+  )
+}
